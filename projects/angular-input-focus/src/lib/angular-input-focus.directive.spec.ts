7f7fdea47ecb71494716e37d35b1372bcbf0bcb2
Dean Davidson <ddavidson85@gmail.com>
2019-06-29 18:07:38 -0700

    Fix tests

--- projects/angular-input-focus/src/lib/angular-input-focus.directive.spec.ts
+++ projects/angular-input-focus/src/lib/angular-input-focus.directive.spec.ts
@@ -1,11 +1,11 @@
 import { AngularInputFocusDirective } from './angular-input-focus.directive';
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
-import { ElementRef, EventEmitter, Component, DebugElement } from '@angular/core';
+import { ElementRef, EventEmitter, Component, DebugElement, PLATFORM_ID } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
 export class MockElementRef extends ElementRef {
   constructor() {
-     super(null);
+    super(null);
   }
 }
 
@@ -20,59 +20,72 @@ class TestComponent {
   public focusEvent = new EventEmitter<boolean>();
 }
 
-const mockNgZone = jasmine.createSpyObj('mockNgZone', ['run', 'runOutsideAngular']);
-mockNgZone.run.and.callFake(fn => fn());
-
-let elementRef: ElementRef;
 let inputs: DebugElement[];
 let fixture: ComponentFixture<TestComponent>;
 
-beforeEach(async(() => {
-  fixture = TestBed.configureTestingModule({
-    declarations: [AngularInputFocusDirective, TestComponent],
-    providers: [
-      { provide: ElementRef, useValue: MockElementRef }
-    ]
-  }).createComponent(TestComponent);
-  fixture.detectChanges(); // initial binding
-  // all elements with an attached HighlightDirective
-  inputs = fixture.debugElement.queryAll(By.directive(AngularInputFocusDirective));
-  elementRef = TestBed.get(ElementRef);
-}));
-
-describe('AngularInputFocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AngularInputFocusDirective(elementRef, mockNgZone);
-    expect(directive).toBeTruthy();
-  });
-
-  it('should auto focus first element', () => {
-    const state = getFocusState();
-    expect(state.first).toBe(true);
-    expect(state.second).toBe(false);
-  });
-
-  it('should allow event-based focus', () => {
-    fixture.componentInstance.focusEvent.emit(true);
-    const state = getFocusState();
-    expect(state.first).toBe(false);
-    expect(state.second).toBe(true);
-  });
-
-  it('should allow event-based blur', () => {
-    // Set focus on second input to remove from first
-    fixture.componentInstance.focusEvent.emit(true);
-    // Now blur second input
-    fixture.componentInstance.focusEvent.emit(false);
-    const state = getFocusState();
-    expect(state.first).toBe(false);
-    expect(state.second).toBe(false);
-  });
-});
-
 function getFocusState() {
-  return { 
+  return {
     first: inputs[0].nativeElement === document.activeElement,
     second: inputs[1].nativeElement === document.activeElement
   }
 }
+
+describe('AngularInputFocusDirective', () => {
+  describe('Browser Tests', () => {
+    beforeEach(async(() => {
+      fixture = TestBed.configureTestingModule({
+        declarations: [AngularInputFocusDirective, TestComponent]
+      }).createComponent(TestComponent);
+      fixture.detectChanges(); // initial binding
+      // all elements with an attached HighlightDirective
+      inputs = fixture.debugElement.queryAll(By.directive(AngularInputFocusDirective));
+    }));
+    it('should auto focus first element', () => {
+      const state = getFocusState();
+      expect(state.first).toBe(true);
+      expect(state.second).toBe(false);
+    });
+
+    it('should allow event-based focus', () => {
+      fixture.componentInstance.focusEvent.emit(true);
+      const state = getFocusState();
+      expect(state.first).toBe(false);
+      expect(state.second).toBe(true);
+    });
+
+    it('should allow event-based blur', () => {
+      // Set focus on second input to remove from first
+      fixture.componentInstance.focusEvent.emit(true);
+      // Now blur second input
+      fixture.componentInstance.focusEvent.emit(false);
+      const state = getFocusState();
+      expect(state.first).toBe(false);
+      expect(state.second).toBe(false);
+    });
+  });
+
+  describe('Server Tests', () => {
+    beforeEach(async(() => {
+      fixture = TestBed.configureTestingModule({
+        declarations: [AngularInputFocusDirective, TestComponent],
+        providers: [
+          { provide: PLATFORM_ID, useValue: 'server' },
+        ]
+      }).createComponent(TestComponent);
+      fixture.detectChanges(); // initial binding
+      // all elements with an attached HighlightDirective
+      inputs = fixture.debugElement.queryAll(By.directive(AngularInputFocusDirective));
+    }));
+    it('should perform no-op when not running in browser and auto focus is true', () => {
+      const state = getFocusState();
+      expect(state.first).toBe(false);
+      expect(state.second).toBe(false);
+    });
+    it('should perform no-op when not running in browser and event emits', () => {
+      fixture.componentInstance.focusEvent.emit(true);
+      const state = getFocusState();
+      // Should do nothing
+      expect(state.second).toBe(false);
+    });
+  });
+});
--- projects/angular-input-focus/src/lib/angular-input-focus.directive.ts
+++ projects/angular-input-focus/src/lib/angular-input-focus.directive.ts
@@ -1,16 +1,35 @@
-import { Directive, ElementRef, Input, NgZone, OnInit, EventEmitter, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, Input, NgZone, EventEmitter, OnDestroy, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: '[libFocus]'
 })
 
-export class AngularInputFocusDirective implements OnInit, OnDestroy {
+/**
+ * An angular focus directive.
+ * ## Usage
+ * For autofocus-like functionality, use like this:
+ * <input [libFocus]="true"...>
+ * 
+ * You can also pass an `EventEmitter<boolean>` as `setFocus` like so:
+ * <input [libFocus]="false" [setFocus]="focusEvent">`
+ * 
+ * Whenever your `focusEvent` emits a value, your element will focus/blur depending on
+ * whether it is true or false.
+ */
+export class AngularInputFocusDirective implements AfterViewInit, OnDestroy {
+  /**
+   * Indicates whether the current platform is a web browser. Used because our `focus` 
+   * implementation is specific to the DOM. Set in constructor.
+   */
+  private isRunningInBrowser: boolean = null;
+
   /**
    * When set to true, directive will mimick `autofocus` like functionality for your input.
    */
-  @Input('libFocus') focus = true;
+  @Input('libFocus') focus = false;
 
   /**
    * When you emit a true value, the input will gain focus. If false, the input will blur.
@@ -31,6 +50,11 @@ export class AngularInputFocusDirective implements OnInit, OnDestroy {
    * @param focus Should the element have focus?
    */
   private setFocusOnElement(focus: boolean) {
+    // If we aren't in a browser, there's nothing to do since our implementation is DOM-specific.
+    // If we don't have a nativeElement, we have nothing to do either.
+    if (this.isRunningInBrowser === false || !this.el.nativeElement) {
+      return;
+    }
     this.zone.runOutsideAngular(() => {
       if (focus === true) {
         this.el.nativeElement.focus();
@@ -40,11 +64,13 @@ export class AngularInputFocusDirective implements OnInit, OnDestroy {
     });
   }
 
-  constructor(private el: ElementRef, private zone: NgZone) { }
+  constructor(private el: ElementRef, private zone: NgZone, @Inject(PLATFORM_ID) platformId: string) {
+    this.isRunningInBrowser = isPlatformBrowser(platformId);
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     if (this.focus === true) {
-      // On init, if focus is set, focus element to mimick autofocus functionality.
+      // After view init, if focus is set, focus element to mimick autofocus functionality.
       this.setFocusOnElement(true);
     }
   }
