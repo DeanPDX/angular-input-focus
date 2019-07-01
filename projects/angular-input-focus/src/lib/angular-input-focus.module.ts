ff938ee246867b4c0572d21d871c158728ec254a
Dean Davidson <ddavidson85@gmail.com>
2019-07-01 11:20:50 -0700

    Add docs to module

--- projects/angular-input-focus/src/lib/angular-input-focus.module.ts
+++ projects/angular-input-focus/src/lib/angular-input-focus.module.ts
@@ -7,4 +7,16 @@ import { AngularInputFocusDirective } from './angular-input-focus.directive';
   ],
   exports: [AngularInputFocusDirective]
 })
+/**
+ * A module for an angular focus directive.
+ * ## Usage
+ * For autofocus-like functionality, use like this:
+ * <input [libFocus]="true"...>
+ * 
+ * You can also pass an `EventEmitter<boolean>` as `setFocus` like so:
+ * <input [libFocus]="false" [setFocus]="focusEvent">`
+ * 
+ * Whenever your `focusEvent` emits a value, your element will focus/blur depending on
+ * whether it is true or false.
+ */
 export class AngularInputFocusModule { }
